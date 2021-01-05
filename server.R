c9e295ce7fcd99a9a16ee444541e89533a467404
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2021-01-05 22:31:44 +0100

    mockup with map

--- server.R
+++ server.R
@@ -1,12 +1,50 @@
 library(shinymaterial)
+library(plotly)
+
+source("data/db.R")
 
 # define server logic
 server <- function(input, output) {
 
-  # output$caption and output$mpgPlot functions
-  output$selected_indicator_name <- renderText({
-    selected <- input$indicators
-    indicators$selected$name
+  cumulated <- reactive({ input$cumulated })
+
+  output$txt_total_count_var <- renderText({
+    paste("Comptage total de ", indicators[[input$indicators]][["name"]])
+  })
+
+  output$txt_cumulated_count_var <- renderText({
+    paste("Comptage Cumulé de ", indicators[[input$indicators]][["name"]])
+  })
+
+  output$val_total_count_var <- renderText({
+    getIndicatorCount(indicators[[input$indicators]][["column"]], indicators[[input$indicators]][["table"]])
+   })
+
+  output$val_cumulated_count_var <- renderText({
+    getIndicatorCount(indicators[[input$indicators]][["column"]], indicators[[input$indicators]][["table"]])
+  })
+
+  output$val_historic_bars <- renderPlotly({
+    data <- getHistoric(indicators[[input$indicators]][["column"]], indicators[[input$indicators]][["table"]], cumulated())
+    fig <- plot_ly(x = data$date, y = data$indicator_sum, type = "bar", name = "total")
+
+    if (cumulated()) {
+      fig <- add_trace(fig, x = data$date, y = data$cumulative_sum, type = "scatter", mode = "lines+markers", name = "cumulé")
+    }
+    return(fig)
+  })
+
+  geo_segment <- reactive({ input$geo_segment })
+
+  output$map <- renderPlotly({
+    data <- getIndicatorCountBy(indicators[[input$indicators]][["column"]], indicators[[input$indicators]][["table"]],
+                                "id_location","DEPARTAMENT", "dim_location", "ID_LOCATION")
+    geo_conf <- list(fitbounds = "locations", visible = FALSE)
+    fig <- plot_ly()
+    fig <- fig %>% add_trace( type="choropleth", geojson=deparments, locations=data$segment, z=data$indicator_sum,
+                              colorscale="Viridis", featureidkey="properties.nom")
+    fig <- fig %>% layout(geo = geo_conf, title = "Par departement")
+    fig
   })
 
   # generate a plot of the requested variable against mpg
