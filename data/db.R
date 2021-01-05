689903311217ee5b5be2b1ffc44e6e26e91013d5
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2021-01-06 00:05:58 +0100

    dynamic sql query by dates

--- data/db.R
+++ data/db.R
@@ -3,6 +3,9 @@ library(RMySQL)
 
 source("conf/global.R")
 
+deparments <- rjson::fromJSON(file = geolocation$deparments)
+regions <- rjson::fromJSON(file = geolocation$regions)
+
 conn <- dbConnect(
     drv = RMySQL::MySQL(),
     dbname = mysql$dbname,
@@ -10,7 +13,56 @@ conn <- dbConnect(
     username = mysql$username,
     password = mysql$password)
 
-dbGetQuery(conn, "SELECT * FROM dim_age LIMIT 5;")
+getIndicatorCount <- function (column_name, table_name){
+  query <- glue::glue("SELECT SUM({column_name}) as indicator_sum FROM {table_name};")
+  result <- dbGetQuery(conn, query)
+  return(result$indicator_sum)
+}
 
+getIndicatorCountBy <- function (ind_col_name, ind_tbl_name, ind_id, dim_col_name, dim_tbl_name, dim_id){
+  query <- glue::glue("
+    SELECT {dim_tbl_name}.{dim_col_name} AS segment, SUM({ind_tbl_name}.{ind_col_name}) AS indicator_sum
+    FROM {ind_tbl_name}
+    LEFT JOIN {dim_tbl_name} ON {ind_tbl_name}.{ind_id} = {dim_tbl_name}.{dim_id}
+    GROUP BY {dim_tbl_name}.{dim_col_name}
+  ")
+  result <- dbGetQuery(conn, query)
+  return(result)
+}
 
-dbDisconnect(conn)
+getHistoric <- function (column_name, table_name, cumulated = TRUE, groupBy = "m"){
+
+  if (groupBy == "d") {
+    time_expression <- "date(dim_date.DATE_AS_DATE)"
+  }
+  if (groupBy == "w") {
+    time_expression <- "str_to_date(concat(yearweek(dim_date.DATE_AS_DATE), ' Sunday'), '%X%V %W')"
+  }
+  if (groupBy == "m") {
+    time_expression <- "str_to_date(concat(date_format(dim_date.DATE_AS_DATE, '%Y-%m'), '-01'), '%Y-%m-%d')"
+  }
+  if (groupBy == "q") {
+    time_expression <- "str_to_date(concat(year(dim_date.DATE_AS_DATE), '-', ((quarter(dim_date.DATE_AS_DATE) * 3) - 2), '-01'), '%Y-%m-%d')"
+  }
+
+  query <- glue::glue("
+    SELECT {time_expression} AS `date`,
+           sum({table_name}.{column_name}) AS `indicator_sum`
+    FROM {table_name}
+    LEFT JOIN dim_date ON {table_name}.id_date = dim_date.ID_DATE
+    GROUP BY {time_expression}")
+
+  if (cumulated){
+    query <- glue::glue("
+      WITH q AS (
+        {query}
+      )
+      SELECT *, SUM(indicator_sum) OVER(ORDER BY date) as cumulative_sum
+      FROM q
+    ")
+  }
+  result <- dbGetQuery(conn, query)
+  return(result)
+}
+
+#dbDisconnect(conn)
